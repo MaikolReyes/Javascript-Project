80e45c748e87eb5ed8f241ea954271d7e798227e
Maikol Reyes <maikolreyes209@gmail.com>
2022-09-05 23:55:16 -0300

    aplicando fetch con json

--- tienda.js
+++ tienda.js
@@ -21,7 +21,7 @@ function addToCartClicked(event) {
   addItemToShoppingCart(itemTitle, itemPrice, itemImage);
 }
 
-function addItemToShoppingCart(itemTitle, itemPrice, itemImage) {
+function addItemToShoppingCart() {
   const elementsTitle = shoppingCartItemsContainer.getElementsByClassName(
     'shoppingCartItemTitle'
   );
@@ -43,28 +43,7 @@ function addItemToShoppingCart(itemTitle, itemPrice, itemImage) {
 
   // APLICANDO EL DOM
   const shoppingCartRow = document.createElement('div');
-  const shoppingCartContent = `
-  <div class="row shoppingCartItem">
-        <div class="col-6">
-            <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-                <img src=${itemImage} class="shopping-cart-image">
-                <h6 class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${itemTitle}</h6>
-            </div>
-        </div>
-        <div class="col-2">
-            <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
-                <p class="item-price mb-0 shoppingCartItemPrice">${itemPrice}</p>
-            </div>
-        </div>
-        <div class="col-4">
-            <div
-                class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
-                <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
-                    value="1">
-                <button class="btn btn-danger buttonDelete" type="button">X</button>
-            </div>
-        </div>
-    </div>`;
+
   shoppingCartRow.innerHTML = shoppingCartContent;
   shoppingCartItemsContainer.append(shoppingCartRow);
 
@@ -146,3 +125,30 @@ function comprarButtonClicked() {
 
 //OPERADORES
 const carrito = JSON.parse(localStorage.getItem('carrito')) || []
+
+const lista = document.querySelector('#listado')
+
+
+
+fetch('/articles.json')
+.then((res) => res.json())
+.then( (data) =>{
+  data.forEach((producto) => {
+    const li = document.createElement('li')
+    li.innerHTML = `
+    <div class="col-12 col-md-6">
+    <div class="item shadow mb-4">
+        <h3 class="item-title">${producto.description}</h3>
+        <img class="item-image" src="${producto.image}">
+
+        <div class="item-details">
+            <h4 class="item-price">${producto.price}</h4>
+            <button class="item-button btn btn-success addToCart">AGREGAR AL CARRITO</button>
+        </div>
+    </div>
+</div>
+    `
+lista.append(li)
+  })
+})
+
